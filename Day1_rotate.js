178751e3377cda39655ae1115b133f7f3154a5d0
Tushar Dighe <dighetushar654@gmail.com>
2021-08-26 16:19:14 +0530

    rotate  the array

---
+++ Day1_rotate.js
@@ -0,0 +1,11 @@
+function rotate(arr, n) {
+    for(let i = 0; i < n; i++) {
+        let el = arr.shift();
+        arr.push(el);
+    }
+    return arr;
+}
+
+let arr = [1,2,3,4,5];
+var n = 3;
+console.log(rotate(arr, n));
