9b863e261d2960ff9c0c399d52c8a9325bb08cec
Tushar Dighe <dighetushar654@gmail.com>
2021-08-31 19:27:24 +0530

    4th Day

--- Second_Highest_Number/secondhigh.js
+++ Second_Highest_Number/secondhigh.js
@@ -1,12 +1,15 @@
 function secondHigh(arr) {
-    let max = [];
-    for(let i = 0; i <arr.length; i++) {
-        if(arr[i] < arr[i+1]) {
-            max.push(arr[i+1]);
-        }
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp; 
+            } 
     }
-    return max[max.length-2];
   }
+  return arr[arr.length-2];
+}
 
-const arr = [2,5,7,9,8,10];
+const arr = [2,5,9,7,6,4,8];
 console.log(secondHigh(arr));
