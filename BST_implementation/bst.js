175d9f5f2e5dff53ad395b5558a3d5720b517187
Tushar Dighe <dighetushar654@gmail.com>
2021-09-02 21:18:06 +0530

    8th Day

--- BST_implementation/bst.js
+++ BST_implementation/bst.js
@@ -38,15 +38,49 @@ class BST {
             }
         }
     }
+    inorder(node) {
+        if(node !== null) {
+		    this.inorder(node.left);
+		    console.log(node.data);
+		    this.inorder(node.right);
+	    }
+    }
+
+    // Performs preorder traversal of a tree	
+    preorder(node) {
+	    if(node !== null) {
+		    console.log(node.data);
+		    this.preorder(node.left);
+		    this.preorder(node.right);
+	    }
+    }
+
+    // Performs postorder traversal of a tree
+    postorder(node) {
+        if(node !== null) {
+            this.postorder(node.left);
+            this.postorder(node.right);
+            console.log(node.data);
+        }
+    }
+
+    getRootNode() {
+        return this.root;
+    }
+
+
 }
 
 
 
 var bst1 = new BST();
+bst1.add(4);
+bst1.add(2);
 bst1.add(5);
+bst1.add(1);
 bst1.add(3);
 bst1.add(6);
-bst1.add(1);
-bst1.add(4);
 
 console.log(bst1);
+var root = bst1.getRootNode();
+console.log(bst1.preorder(root));
